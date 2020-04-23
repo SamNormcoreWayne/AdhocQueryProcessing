26b3cca3ca2128182332f0fd3f750c17ffcd669e
SamNormcoreWayne <zcvszzy@gmail.com>
2020-04-23 15:40:26 -0400

    Implemented postgresCon.py; add db file sdap.sql

--- main/bin/postgresCon.py
+++ main/bin/postgresCon.py
@@ -5,7 +5,10 @@ import psycopg2 as pstgre
 '''
 
 class postgresCon():
-    def __main__(self, user, pwd, db_name='sales', host='localhost', port='5432'):
+    def __main__(self, user, pwd):
+        db_name = 'sales'
+        host = 'localhost'
+        port = '5432'
         self.db_name = db_name
         self.user = user
         self.pwd = pwd
@@ -14,6 +17,7 @@ class postgresCon():
         self.conn = pstgre.connect(host=host, database=db_name, user=user, password=pwd, port=port)
         self.sqlLst = []
         self.having_conds = []
+        self.dataset = []
 
     def getSql(self, sqlStr : str):
         self.sqlLst.append(sqlStr)
@@ -23,12 +27,14 @@ class postgresCon():
         for sql in self.sqlLst:
             try:
                 self.cur.execute(sql)
+                output = self.cur.fetchone()
             except(pstgre.DatabaseError) as dberr:
                 print("Error while execute query: \n '{}'\n".format(sql), dberr)
 
             finally:
-                if output is not None:
-                    output = self.conn.fetchone()
+                while output is not None:
+                    yield output
+                    output = self.cur.fetchone()
                     # this is unfinished.
         # TODO: store the output in a proper way
 
@@ -38,5 +44,10 @@ class postgresCon():
     def outputSQL(self):
         None
 
+    def closeCur(self):
+        if self.cur.closed is False:
+            self.cur.close()
+
     def closeDB(self):
+        self.closeCur()
         self.conn.close()
